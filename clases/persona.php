beb5cbb88f23c81c51ecc3383cc053e0285df3e1
mmlonamarroquin1987-commits <tu_email@ejemplo.com>
2026-06-25 17:11:36 -0500

    creaccion objecto persona

---
+++ clases/persona.php
@@ -0,0 +1,13 @@
+<?php 
+
+class persona
+{
+public function saludar()
+{
+    echo "¡Hola! Bienvenido a programar orientada a objectos con PHP.";
+}
+
+}
+
+
+?>
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once "../clases/persona.php";
+
+$amparo= new persona();
+$amparo->saludar();
+?>
